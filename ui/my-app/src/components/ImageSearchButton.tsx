df7fc2cd5d947d9df1d49e5f47875384bb39384d
Robbie Zhao <reg.zhao@outlook.com>
2020-10-17 17:54:35 -0400

    added image search component

--- ui/my-app/src/components/ImageSearchButton.tsx
+++ ui/my-app/src/components/ImageSearchButton.tsx
@@ -1,13 +1,30 @@
+import { Button, makeStyles } from '@material-ui/core';
 import React from 'react';
-
+import ImageSearchIcon from '@material-ui/icons/ImageSearch';
 
 interface ImageSearchButtonProps {
 
 }
 
+const useStyles = makeStyles((theme) => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+}));
+
 const ImageSearchButton = (props: ImageSearchButtonProps): React.ReactElement => {
-  return(
-    <div>hello</div>
+  const classes = useStyles();
+
+  return (
+    <Button
+      variant="contained"
+      color="default"
+      className={classes.button}
+      startIcon={<ImageSearchIcon />}
+      onClick = {() => {alert('t r i g g e r e d')}}
+    >
+      Image Search
+    </Button>
   )
 }
 
