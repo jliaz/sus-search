012bcc9d8e5027341ee5fa440f0a9d7a51bbfd83
Robbie Zhao <reg.zhao@outlook.com>
2020-10-17 17:16:32 -0400

    added search component

--- ui/my-app/src/components/Search.tsx
+++ ui/my-app/src/components/Search.tsx
@@ -1,3 +1,6 @@
+import classes from '*.module.css';
+import { IconButton, InputBase, makeStyles, Paper, } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
 import React from 'react';
 
 
@@ -5,9 +8,35 @@ interface SearchProps {
 
 }
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: '2px 4px',
+    display: 'flex',
+    alignItems: 'center',
+    width: 400,
+  },
+  input: {
+    marginLeft: theme.spacing(1),
+    flex: 1,
+  },
+  iconButton: {
+    padding: 10,
+  },
+}));
+
 const Search = (props: SearchProps): React.ReactElement => {
-  return(
-    <div>hello</div>
+  const classes = useStyles();
+
+  return (
+    <Paper component="form" className={classes.root}>
+      <InputBase
+        className={classes.input}
+        placeholder="Search..."
+      />
+      <IconButton type="submit" className={classes.iconButton} onClick={() => { alert("sus") }}>
+        <SearchIcon />
+      </IconButton>
+    </Paper>
   )
 }
 
