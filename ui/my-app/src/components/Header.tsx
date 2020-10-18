c0a90c2161170b3a2b1da0e0bbad8a8f7fe85b8e
Robbie Zhao <reg.zhao@outlook.com>
2020-10-18 04:44:51 -0400

    added link to home for logo

--- ui/my-app/src/components/Header.tsx
+++ ui/my-app/src/components/Header.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import logo from '../assets/logo.png';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
-import { Grid, makeStyles } from '@material-ui/core'
+import { Grid, makeStyles } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+
 
 interface HeaderProps {
   inAppBar?: boolean
@@ -18,22 +20,25 @@ const Header = (props: HeaderProps): React.ReactElement => {
   const classes = useStyles();
   const { inAppBar } = props;
   return (
-    <Grid
-      container
-      justify="flex-start"
-      alignItems="center"
-    >
-      <Grid item>
-        <img src={logo} alt="Logo" style={{ width: inAppBar ? "40px" : "60px" }} />
-      </Grid>
-      <Grid item>
-        <Typography component="h6" color="secondary" className={classes.typography}>
-          <Box fontSize={inAppBar ? "h6.fontSize" : "h3.fontSize"} m={1}>
-            SusSearch
+    <Link to="/" style={{ textDecoration: 'none' }}>
+      <Grid
+        container
+        justify="flex-start"
+        alignItems="center"
+      >
+        <Grid item>
+          <img src={logo} alt="Logo" style={{ width: inAppBar ? "40px" : "60px" }} />
+        </Grid>
+        <Grid item>
+          <Typography component="h6" color="secondary" className={classes.typography}>
+            <Box fontSize={inAppBar ? "h6.fontSize" : "h3.fontSize"} m={1}>
+              SusSearch
                 </Box>
-        </Typography>
+          </Typography>
+        </Grid>
       </Grid>
-    </Grid>
+    </Link>
+
   )
 }
 
