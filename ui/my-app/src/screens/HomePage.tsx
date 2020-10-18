9733f0280d8ce0f39bb1dd39d426b00c49eabfd8
Robbie Zhao <reg.zhao@outlook.com>
2020-10-18 00:38:20 -0400

    added card component

--- ui/my-app/src/screens/HomePage.tsx
+++ ui/my-app/src/screens/HomePage.tsx
@@ -4,6 +4,7 @@ import Search from '../components/Search';
 import ImageSearchButton from '../components/ImageSearchButton';
 import Header from '../components/Header';
 import { Grid, GridListTileBar, makeStyles } from '@material-ui/core';
+import ProductCard from '../components/ProductCard';
 
 interface HomePageProps {
 
@@ -23,26 +24,35 @@ const HomePage = (props: HomePageProps): React.ReactElement => {
   const classes = useStyles();
 
   return (
-    <Grid
-      container
-      justify="center"
-      alignItems="center"
-      className={classes.root}
-    >
-      <Grid item className={classes.header}>
-        <Header />
-      </Grid>
+    <div>
       <Grid
         container
-        item
-        direction="row"
         justify="center"
         alignItems="center"
+        className={classes.root}
       >
-        <Search></Search>
-        <ImageSearchButton></ImageSearchButton>
+        <Grid item className={classes.header}>
+          <Header />
+        </Grid>
+        <Grid
+          container
+          item
+          direction="row"
+          justify="center"
+          alignItems="center"
+        >
+          <Search></Search>
+          <ImageSearchButton></ImageSearchButton>
+        </Grid>
       </Grid>
-    </Grid>
+
+      <ProductCard
+        productName="Green Lizard"
+        companyName="The Lizard Company"
+        price={125}
+        productLink="https://www.nationalgeographic.com/animals/reptiles/"
+        imageLink="https://www.nationalgeographic.com/content/dam/animals/pictures/hero/reptiles-hero.jpg"></ProductCard>
+    </div>
 
 
   )
